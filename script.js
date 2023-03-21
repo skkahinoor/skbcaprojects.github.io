d17f095707b5e4f10ce61a761aa4767dcb2a8d7a
Coding4-Everyone <96863031+Coding4-Everyone@users.noreply.github.com>
2023-03-22 03:39:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var typed = new Typed(".auto-input", {
-  strings: ["Gulzar", "Web Developer", "Programmer"],
-  typeSpeed: 100,
-  bcakSpeed: 100,
-  loop: true,
+AOS.init({
+  offset: 300,
 });
+// var typed = new Typed(".auto-input", {
+//   strings: ["Gulzar", "Web Developer", "Programmer"],
+//   typeSpeed: 100,
+//   bcakSpeed: 100,
+//   loop: true,
+// });
